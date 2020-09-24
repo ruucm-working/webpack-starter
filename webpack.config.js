686d579e1f057cf328a4564cffc326e9381c076a
ruucm <ruucm.a@gmail.com>
2020-09-24 18:23:25 +0900

    add server (webpack-dev-server)
    but index.html is ignored

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  watch: true,
-  plugins: [new CleanWebpackPlugin({ cleanStaleWebpackAssets: false })],
 };
